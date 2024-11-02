0db6b5bc7800950569976669d044a8bbbebf3738
Isha <isha.khurana2610@gmail.com>
2024-11-02 23:52:53 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChw1o5c8J8bS9SdoVDV6WFT2St9WZWPK4",
+  authDomain: "cros-45717.firebaseapp.com",
+  projectId: "cros-45717",
+  storageBucket: "cros-45717.firebasestorage.app",
+  messagingSenderId: "924762746061",
+  appId: "1:924762746061:web:62a2f9d6911c41d9384d84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
